a279b2c3b0882f1a837998be8c7beda4127a6da0
XavierM21 <64445609+XavierM21@users.noreply.github.com>
2021-02-11 10:39:55 -0500

    Initial commit

---
+++ Models/player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace engine.Models
+{
+    public class player // public is there to make this class and vars visiable to the entire program.
+    {
+        // get and set means that you can get the value and set the value of the string and int
+        public string Name { get; set; }
+        public string CharClass { get; set; }
+        public int HP { get; set; }
+        public int XP { get; set; }
+        public int Lvl { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ ViewModels/GameSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using engine.Models;
+
+namespace engine.ViewModels
+{
+    class GameSession
+    {
+        player CurrentPlayer { get; set; }
+        
+        public GameSession()
+        {
+            CurrentPlayer = new player(); // creates a new player object and put that on line 10(playre CurrentPlayer { get; set; }
+            CurrentPlayer.Name = "xav";
+            CurrentPlayer.Gold = 1000000;
+
+        }
+    }
+}
