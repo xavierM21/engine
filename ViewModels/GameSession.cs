8de6e39ad2c4fc90f650fb0b55ad8013a304807a
XavierM21 <64445609+XavierM21@users.noreply.github.com>
2021-03-24 20:54:23 -0400

    update
    -added more player info

--- ViewModels/GameSession.cs
+++ ViewModels/GameSession.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using System.Text;
 using engine.Models;
-
 namespace engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        player CurrentPlayer { get; set; }
+        public player CurrentPlayer { get; set; }
         
         public GameSession()
         {
             CurrentPlayer = new player(); // creates a new player object and put that on line 10(playre CurrentPlayer { get; set; }
             CurrentPlayer.Name = "xav";
             CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.HP = 10;
+            CurrentPlayer.CharClass = "fighter";
+            CurrentPlayer.Lvl = 1;
+            CurrentPlayer.XP = 0;
 
         }
     }
